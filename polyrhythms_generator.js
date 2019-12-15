71ccc0a304f1d452f8dd328a8d9372ed6166773f
Alessio Lampis <alelampis2295@gmail.com>
2019-12-15 12:03:35 +0100

    title_trial

--- polyrhythms_generator.js
+++ polyrhythms_generator.js
@@ -3,9 +3,12 @@ var host = document.getElementById("host");
 var tatum = 1;
 var guest_circle = { tatum: 16 };
 var host_circle = { tatum: 16 };
-var canvas = document.getElementById('stage');
 const container = document.getElementById("guest_circle_on_screen");
 
+//ANIMATION
+
+// Canvas
+var canvas = document.getElementById('stage');
 
 // 2d Drawing Context.
 var ctx = canvas.getContext('2d');
@@ -24,7 +27,7 @@ function drawCircle() {
     if (angle <= Math.PI) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        var radius = 45 * Math.abs(Math.cos(angle));
+        var radius = 15 * Math.abs(Math.cos(angle));
         ctx.arc(50, 50, radius, 0, 2 * Math.PI, false);
         ctx.closePath();
         angle += pixelsPerFrame / 20;
@@ -41,54 +44,83 @@ function drawCircle() {
 
 
 guest.onchange = function () {
-    document.getElementById("guest").value = guest.value;
+    document.getElementById("guest").value = Math.floor(guest.value);
     tatum_calculation();
     if (guest.value > 36) {
         guest.value = 36;
         alert("Guest value can't exceed 36");
-    }
+    };
+    if (guest.value == 0) {
+        guest.value = 1;
+    };
+
 
 
 }
 
 host.onchange = function () {
-    document.getElementById("host").value = host.value;
+    document.getElementById("host").value = Math.floor(host.value);
     tatum_calculation();
     if (host.value > 36) {
         host.value = 36;
         alert("Host value can't exceed 36")
-    }
+    };
+    if (host.value == 0) {
+        host.value = 1;
+    };
 }
 
 function tatum_calculation() {
-    tatum = host.value * guest.value;
+    tatum = lcm_two_numbers(Math.floor(host.value), Math.floor(guest.value));
     result.innerHTML = tatum;
     guest_circle.tatum = tatum;
     host_circle.tatum = tatum;
 
 }
 
-function show_tatum() {
+function lcm_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+        return false;
+    return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+};
 
+function gcd_two_numbers(x, y) {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    return x;
+};
+
+
+function show_tatum() {
     if (guest_circle_on_screen.childElementCount != 0) {
         circles = container.querySelectorAll(".sub");
         circles.forEach((circle) => circle.remove())
-    }
-    ;
+    };
+
+
     for (var x = 0; x < tatum; x++) {
         add_circle()
     }
-}
+};
+
 
 function add_circle() {
     el = document.createElement("div")
     el.classList.add("sub")
     guest_circle_on_screen.appendChild(el)
-}
+};
 
 
 StartButton.onclick = function () {
     show_tatum();
     drawCircle()
 };
+
+
+
 
