2cd83612595008a8a8385fbe98cf94c1d6028410
Alessio Lampis <alelampis2295@gmail.com>
2020-01-17 18:13:46 +0100

    17-01-2020

--- polyrhythms_generator.js
+++ polyrhythms_generator.js
@@ -1,11 +1,10 @@
-var guest = document.getElementById("guest");
-var host = document.getElementById("host");
-var tatum = 1;
+var guest = document.getElementById("guest1");
+var host = document.getElementById("host1");
+var tatum = { tatum1: 1, tatum2: 1, tatum3: 1 };
 const container = document.getElementById("guest_circle_on_screen");
 var host_colors = [];
 var guest_colors = [];
 var CurrentPage = 0;
-
 var canvas = document.getElementById("stage");
 var ctx = canvas.getContext("2d");
 
@@ -17,20 +16,68 @@ var requestAnimationFrame =
 var pixelsPerFrame = 5;
 var angle = Math.PI / 2;
 
+//BACKGROUND
+let Btn = document.getElementsByClassName("firstbtn");
+Btn[0].onclick = function(){ document.getElementsByClassName("page")[0].style.display = "block"; document.getElementsByClassName("page")[0].classList.add("animate-in");
+setTimeout(function(){document.getElementsByClassName("page")[0].classList.remove("animate-in");
+                      
+}, 600);
+document.getElementsByClassName("firstpage")[0].style.display = "none"};
+
 //VIEW PAGE
-function ShowPage(n){
+
+function ShowPage(n) {
+  guest = document.getElementById("guest" + (n + 1));
+  host = document.getElementById("host" + (n + 1));
   var x = document.getElementsByClassName("page");
   var buttons = document.getElementsByTagName("button");
   x[CurrentPage].style.display = "none";
-  buttons[CurrentPage].style.backgroundColor = "";
+//buttons[CurrentPage].style.backgroundColor = "";
   CurrentPage = n;
   x[n].style.display = "block";
-  buttons[n].style.backgroundColor = "blue";
+  //buttons[n].style.backgroundColor = "blue";
 };
+
+var selectors = document.querySelectorAll("button");
+selectors[0].style.background = "black";
+selectors[0].style.color = "#dce1d5";
+
+function color_button(event){
+  selectors.forEach(reset_buttons);
+  if(event.target.className == "LinBtn"){
+    ShowPage(0);
+    selectors[0].style.background = "black";
+    selectors[0].style.color = "#dce1d5";
+  };
+  if(event.target.className == "IrrBtn"){
+    ShowPage(1);
+    selectors[4].style.background = "black";
+    selectors[4].style.color = "#dce1d5";
+  };
+  if(event.target.className == "ClvBtn"){
+    ShowPage(2);
+    selectors[8].style.background = "black";
+    selectors[8].style.color = "#dce1d5";
+  };
+  
+};
+
+function reset_buttons(selector){
+  selector.style.background ="#dce1d5" ;
+selector.style.color = "black";
+};
+
+function select_button(selector){
+  selector.onclick = color_button;
+};
+
+selectors.forEach(select_button);
 
 //TATUM
 guest.onchange = function() {
-  document.getElementById("guest").value = Math.floor(guest.value);
+  document.getElementById("guest" + (CurrentPage + 1)).value = Math.floor(
+    guest.value
+  );
   if (guest.value > 8) {
     guest.value = 8;
     alert("Guest value can't exceed 8");
@@ -42,7 +89,67 @@ guest.onchange = function() {
 };
 
 host.onchange = function() {
-  document.getElementById("host").value = Math.floor(host.value);
+  document.getElementById("host" + (CurrentPage + 1)).value = Math.floor(
+    host.value
+  );
+
+  if (host.value > 8) {
+    host.value = 8;
+    alert("Host value can't exceed 8");
+  }
+  if (host.value == 0) {
+    host.value = 1;
+  }
+  tatum_calculation();
+};
+
+guest2.onchange = function() {
+  document.getElementById("guest" + (CurrentPage + 1)).value = Math.floor(
+    guest.value
+  );
+  if (guest.value > 8) {
+    guest.value = 8;
+    alert("Guest value can't exceed 8");
+  }
+  if (guest.value == 0) {
+    guest.value = 1;
+  }
+  tatum_calculation();
+};
+
+host2.onchange = function() {
+  document.getElementById("host" + (CurrentPage + 1)).value = Math.floor(
+    host.value
+  );
+
+  if (host.value > 8) {
+    host.value = 8;
+    alert("Host value can't exceed 8");
+  }
+  if (host.value == 0) {
+    host.value = 1;
+  }
+  tatum_calculation();
+};
+
+guest3.onchange = function() {
+  document.getElementById("guest" + (CurrentPage + 1)).value = Math.floor(
+    guest.value
+  );
+  if (guest.value > 8) {
+    guest.value = 8;
+    alert("Guest value can't exceed 8");
+  }
+  if (guest.value == 0) {
+    guest.value = 1;
+  }
+  tatum_calculation();
+};
+
+host3.onchange = function() {
+  document.getElementById("host" + (CurrentPage + 1)).value = Math.floor(
+    host.value
+  );
 
   if (host.value > 8) {
     host.value = 8;
@@ -55,8 +162,11 @@ host.onchange = function() {
 };
 
 function tatum_calculation() {
-  tatum = lcm_two_numbers(Math.floor(host.value), Math.floor(guest.value));
-  result.innerHTML = tatum;
+  tatum["tatum" + (CurrentPage + 1)] = lcm_two_numbers(
+    Math.floor(host.value),
+    Math.floor(guest.value)
+  );
+  result[CurrentPage].innerHTML = tatum["tatum" + (CurrentPage + 1)];
 }
 
 function lcm_two_numbers(x, y) {
@@ -88,19 +198,6 @@ document.body.onkeydown = function(e) {
     drawCircle();
   }
 };
-
-//CIRCLE
-
-function set_tatum(x, y) {
-  host_colors = [];
-  guest_colors = [];
-  host_colors.push("black");
-  guest_colors.push("black");
-  for (var i = 0; i < tatum; i++) {
-    if (i == guest.value) {
-    }
-  }
-}
 
 //ANIMATION
 
