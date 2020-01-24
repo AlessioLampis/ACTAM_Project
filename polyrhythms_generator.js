c16443bff55c89a546bfa5025c0c19615059f960
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2020-01-24 16:50:34 +0100

    Update polyrhythms_generator.js
    mattia gay

--- polyrhythms_generator.js
+++ polyrhythms_generator.js
@@ -12,7 +12,7 @@
 // ACCEPT ONLY POSITIVE VALUES ON HOST AND HUEST!!
 // Sometimes pies stop to rotate
 // do a research about visualizing polyrhythm in circular fashion
-
+// MATTIA GAY
 
 ///
 //**MODEL**//
